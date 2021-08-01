c33e2430e73fcbcfa3a6491bac806a56d10ff90b
HassanGG <hassan.g.ghnewa@gmail.com>
2021-08-01 19:26:55 +0100

    added intro text

--- Linear/graphics.js
+++ Linear/graphics.js
@@ -1,11 +1,12 @@
 const points = [];
 const fit = { x1: 0, y1: 0, x2: 1, y2: 0 };
-const diameter = 8;
+const pointDiameter = 8;
+const introLocation = {x: 0.4, y: 0.5};
+const introSize = 33;
 
 // adds a point on the screen
 function pushPoint(X, Y) {
     points.push({ x: X, y: Y });
-    console.log(tf.memory());
 }
 
 // draws all the points
@@ -16,7 +17,7 @@ function drawPoints() {
         // multiply by width/height because it is normalized
         let x = points[i].x * width;
         let y = points[i].y * height;
-        circle(x, y, diameter);
+        circle(x, y, pointDiameter);
     }
 }
 
@@ -31,12 +32,22 @@ function setup() {
 function draw() {
     background(27, 31, 38);
     drawPoints();
-    drawFit();
     if(points.length > 0) {
         runNetwork();
+        drawFit();
+    } else {
+        drawIntro();
     }
 }
 
+function drawIntro() {
+    textSize(introSize);
+    let x = introLocation.x * width;
+    let y = introLocation.y * height;
+    fill(235, 161, 52);
+    text("Click anywhere to start.", x, y);
+
+}
 // changes the canvas size on window resize
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
