eb33be80b6c4c50c10cefe27e53b188ca4c1168c
HassanGG <hassan.g.ghnewa@gmail.com>
2021-08-01 17:24:04 +0100

    added line drawing functionality using y=mx+b

--- graphics.js
+++ graphics.js
@@ -1,4 +1,5 @@
 const points = [];
+const fit = { x1: 0, y1: 0, x2: 1, y2: 0 };
 const diameter = 8;
 
 // adds a point on the screen
@@ -8,6 +9,7 @@ function pushPoint(X, Y) {
 
 // draws all the points
 function drawPoints() {
+    strokeWeight(0);
     fill(211, 223, 242);
     for (let i = 0; i < points.length; i++) {
         // multiply by width/height because it is normalized
@@ -22,12 +24,14 @@ function setup() {
     var cnv = createCanvas(windowWidth, windowHeight);
     cnv.style('display', 'block');
     background(27, 31, 38);
+    pushFit(1, 0.1);
 }
 
 // draws everything
 function draw() {
     background(27, 31, 38);
     drawPoints();
+    drawFit();
 }
 
 // changes the canvas size on window resize
@@ -44,6 +48,20 @@ function mouseClicked() {
     console.log(points);
 }
 
-function plotLine(x, y) {
+// takes in numbers between 1 and 0 and plots it on canvas
+function pushFit(m, b) {
+    fit.y1 = (m * fit.x1) + b;
+    fit.y2 = (m * fit.x2) + b;
+    console.log(fit);
+}
 
+function drawFit() {
+    stroke(235, 61, 52);
+    strokeWeight(3);
+    // height - x because y = 0, is at the top instead of bottom.
+    let x1 = fit.x1 * width;
+    let y1 = height - (fit.y1 * height);
+    let x2 = fit.x2 * width;
+    let y2 = height - (fit.y2 * height);
+    line(x1, y1, x2, y2);
 }
--- linear.js
+++ linear.js
@@ -1,1 +1,2 @@
 // data is contained within points[].
+
